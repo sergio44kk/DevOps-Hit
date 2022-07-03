033119705561db4aa78a6636e7c0f3390ed0bc8f
sergey <sergey.rmain@gmail.com>
2022-07-03 19:40:43 +0300

    hit

---
+++ .idea/hello.java
@@ -0,0 +1,30 @@
+package com.example.helloworld;
+
+import java.util.concurrent.ExecutionException;
+
+public class HelloWorld {
+    public static void main(String[] arg) {
+        System.out.println("hello world");
+        int i = 0;
+        while(i <= 5){
+            System.out.println(i);
+            i++;
+        }
+        String name = "test";
+        for(int j = 0; j <= 3;j++){
+            System.out.printf("this is J: %d %s \n",j, name);
+        }
+        try {
+            int myNumber = 10 / 0;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+
+            System.out.println(e);
+
+        }
+        throw new RuntimeException("exp test");
+    }
+}
+
